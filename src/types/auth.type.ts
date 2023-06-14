3d60c329acbcb8a237e0ffd94f9bab1e053b7434
Tran Hai Trieu <haitrieu2524@gmail.com>
2023-06-14 13:00:22 +0700

    Sửa đổi lại các file chuẩn bị deploy với Vercel

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -1,5 +1,5 @@
 import { User } from './user.type';
-import { ErrorResponse, SuccessResponse } from './utils.type';
+import { SuccessResponse } from './utils.type';
 
 export type AuthResponse = SuccessResponse<{
   access_token: string;
